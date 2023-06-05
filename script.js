73a3a2a38aa4407475caaa3fb8dda4ee997cb229
theallSparky <harrisonsparks.1999@gmail.com>
2023-06-05 03:36:34 -0500

    Added basic functionality for the image of the pokemon the load upon searching for it.

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("working")
+
+let button = document.querySelector("#searchButton")
+
+button.addEventListener('click', async () => {
+
+    let pokemonName = document.querySelector("#pokemonName")
+    let pokemonImage = document.querySelector("#pokemonImage")
+    let pokemonSpecies = document.querySelector("#pokemonSpecies")
+    let pokemonDescription = document.querySelector("#pokemonDescription")
+
+    let textInput = document.querySelector("#inputBar").value
+
+    //Axios call goes here
+    let pokemonResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${textInput}`) // Selecting the pokemon
+    console.log(pokemonResponse)
+
+    let pokeName = pokemonResponse.data.name
+    console.log(pokeName)
+    let pokePic = pokemonResponse.data.sprites.front_default
+    console.log(pokePic)
+    //remember to use Async and Await!
+    //DOM Setters go here
+    pokemonImage.src= pokePic
+})
